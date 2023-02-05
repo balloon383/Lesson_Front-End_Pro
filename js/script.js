e644c0a89712b236756b545ccdd886a5132b11ce
Anton Chekalenko <aston23112001@gmail.com>
2023-02-05 16:56:00 +0200

    lesson-4 changes

--- js/script.js
+++ js/script.js
@@ -1,34 +1,55 @@
-let userEmail = prompt('Enter your email')
-let userPassword = prompt('Enter your password') 
+let userEmail = prompt("Enter your email");
+let userPassword = prompt("Enter your password"); 
 let errors
 let tryCount = 3
-/* login */
-if (userEmail.endsWith('.com') == false) {
-    errors = 'email should end with .com,'
-} else if(userEmail.includes('@') == 0){
-    errors = 'email should include one @,'
-} else if (userEmail.startsWith('@') == true) {
-    errors = 'email should not start with @,'
-} else if (userEmail.endsWith('@') == true) {
-    errors = 'email shoud not end with @'
-} else if (userEmail.length > 15) {
-    errors = 'email lenght should not be more than 15 symbols,'
-} else {
-    errors = true
-}
-/* password */
-if (userPassword.charAt(0).charCodeAt() < 65 || userPassword.charAt(0).charCodeAt() > 90) {
-    errors =  'password should start with big letter'
-} else if(userPassword.length < 4 || userPassword.length > 12){
-    errors = 'password should be longer than 4 symbols or shorter than 12 symbols'
-} else {
-errors = true
-} 
+/* uslovia
+email
+(userEmail.endsWith('.com') == false)
+(userEmail.includes('@') == 0)
+(userEmail.startsWith('@') == true)
+(userEmail.endsWith('@') == true)
+(userEmail.length > 15)
+password
+(userPassword.charAt(0).charCodeAt() < 65 || userPassword.charAt(0).charCodeAt() > 90)
+(userPassword.length < 4 || userPassword.length > 12)
+*/
 
-while (tryCount > 0 || (errors)) {
+    
 
+do {
     tryCount = tryCount - 1
-} 
+    if (userEmail.endsWith(".com") == false) {
+        userEmail = prompt('your email should ends with .com')
+    } else {
+        errors = false
+    }
+    if (userEmail.includes("@") == 0) {
+        userEmail = prompt("your email shoul contain at least one @");
+    } else {
+        errors = false;
+    }
+    if (userEmail.startsWith("@") == true || userEmail.endsWith("@") == true) {
+        userEmail = prompt("your email shoul not starts or ends with @");
+    } else {
+        errors = false;
+    }
+    if (
+        userPassword.charAt(0).charCodeAt() < 65 || userPassword.charAt(0).charCodeAt() > 90
+    ) {
+        userPassword = prompt("your password should start with big letter");
+    } else {
+        errors = false
+    }
+    if (userPassword.length < 4 || userPassword.length > 12) {
+        userPassword = prompt("password length should be longer than 4 symbols but shorter than 12");
+    } else {
+        errors = false
+    }
+    if (tryCount == 0) {
+        alert("no tries left");
+    } else {
+        alert(`tries left: ${tryCount}`);
+    }
+} while (tryCount > 0 || (errors))
 
-
-
+document.write(`account successfully registered user email: ${userEmail}, user password: ${userPassword}`)
