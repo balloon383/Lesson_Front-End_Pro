1cd7939d443420993ccf4075c4db87dc3818a36a
Anton Chekalenko <aston23112001@gmail.com>
2023-02-06 11:05:58 +0200

    added regex password validation

--- js/script.js
+++ js/script.js
@@ -5,45 +5,43 @@ let tryCount = 3;
 let isValide
 
 do {
+    debugger
     tryCount = tryCount - 1;
     errors = ``;
-    userEmail = prompt(`Enter your Email`)
-    userPassword = prompt(`Enter your Password`)
+    userEmail = prompt(`Enter your Email`);
+    userPassword = prompt(`Enter your Password`);
+        
+    if (userEmail.endsWith(".com") == false) {
+        errors = `email should ends with .com,`;
+    }
+    if (userEmail.includes("@") == 0) {
+        errors = ` ${errors} email should include one @,`;
+    }
+    if (userEmail.startsWith("@") == true || userEmail.endsWith("@") == true) {
+        errors = ` ${errors} email should not start or end with @,`;
+    }
+    if (userPassword.search(/[A-Z]/g) == -1) {
+        errors = `${errors} password should contain at least one uppercase letter,`;
+    }
+    if (userPassword.length < 4 || userPassword.length > 12) {
+        errors = `${errors}password should be longer than 4 symbols but shorter than 12.`;
+    }
 
-if (userEmail.endsWith(".com") == false) {
-    errors = `email should ends with .com,`;
-} 
-if (userEmail.includes("@") == 0) {
-    errors = ` ${errors} email should include one @,`;
-} 
-if (userEmail.startsWith("@") == true || userEmail.endsWith("@") == true) {
-    errors = ` ${errors} email should not start or end with @,`;
-} 
-if (
-    userPassword.charAt(0).charCodeAt() < 65 ||
-    userPassword.charAt(0).charCodeAt() > 90
-) {
-    errors = `${errors} password should start with big letter,`;
-} 
-if (userPassword.length < 4 || userPassword.length > 12) {
-    errors = `${errors}password should be longer than 4 symbols but shorter than 12.`;
-} 
-
-if (errors == false) {
-    tryCount = tryCount - tryCount
-    isValide = true
-} else {
-    alert(errors)
-    if (tryCount == 0) {
-        alert(`no tries left!`)
-        isValide = false;
+        if (errors == false) {
+        tryCount = tryCount - tryCount;
+        isValide = true;
     } else {
+        alert(errors);
+        if (tryCount == 0) {
+        alert(`no tries left!`);
+        isValide = false;
+        } else {
         alert(`tries left: ${tryCount}`);
     }
-}
-if (tryCount == 0){
-    errors = false
-}
+    }
+    if (tryCount == 0) {
+        errors = false;
+    }
 } while (tryCount > 0 || errors);
 if (isValide == true) {
     document.write(`Account successfully registered! User email: ${userEmail}, User password: ${userPassword}`);
