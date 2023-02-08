102ba8a128e8c20396059761d125baf007516e55
Anton Chekalenko <aston23112001@gmail.com>
2023-02-08 12:56:19 +0200

    but fixes

--- js/script.js
+++ js/script.js
@@ -1,22 +1,20 @@
-let randomValue = Math.round(Math.random() * 10);
-console.log(randomValue)
 const arr = []
+let result
 let arrayLength = +prompt('enter array length');
 
-while (arrayLength < 2) {
-    arrayLength = +prompt('array length must be longer than 2')
-}
-while (arrayLength > 10) {
-    arrayLength = +prompt("array length must be but shorter than 10");
-}
 arrayLength = Math.abs(arrayLength);
-arrayLength = Math.round(arrayLength)
+arrayLength = Math.round(arrayLength);
+
+while (arrayLength > 10 || arrayLength < 2) {
+    arrayLength = +prompt("array length must be but shorter than 10 but longer than 2");
+}
+
 for (a = 0; arrayLength > a; a++){
     arr.push(Math.round(Math.random() * 10));
     console.log(arr);
 }
 console.log(arr);
-let result = arr.reduce((a, b) => {
+result = arr.reduce((a, b) => {
     return a + b;
 })
 console.log(result)
