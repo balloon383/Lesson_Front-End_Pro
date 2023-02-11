7dbe0b4d458ad3649a8406c8f0b9eb7c0f5abdd9
Anton Chekalenko <aston23112001@gmail.com>
2023-02-11 14:45:38 +0200

    bug fixes lesson 6

--- js/script.js
+++ js/script.js
@@ -9,9 +9,6 @@ let sumPositiveArr = positiveArr.reduce((a, b) => {
     return a + b;
 });
 console.log(positiveArr, sumPositiveArr);
-
-
-
 // 2 Знайти мінімальний елемент масиву та його порядковий номер.
 let arrId;
 let arrMin = arr.reduce((a, b) => {
@@ -19,33 +16,22 @@ let arrMin = arr.reduce((a, b) => {
 })
 let idMin = arr.indexOf(arrMin)
 console.log(arrMin, idMin);
-
-
 // 3 Знайти максимальний елемент масиву та його порядковий номер.
 let arrMax = arr.reduce((a, b) => {
     return Math.max(a, b);
 });
 let idMax = arr.indexOf(arrMax);
 console.log(arrMax, idMax);
-
-
-
 // 4 Визначити кількість негативних елементів.
 let negativeArr = arr.filter((el) => {
     return el < 0 
 })
-
-
-
-
+console.log(negativeArr)
 // 5 Знайти кількість непарних позитивних елементів.
 let oddPosArr = arr.filter((el) => {
     return el % 2 > 0
 })
 console.log(oddPosArr);
-
-
-
 // 6 Знайти кількість парних позитивних елементів.
 let pairArr = arr.filter((el) => {
     return (el % 2 == 0)
@@ -54,16 +40,11 @@ let pairPosArr = pairArr.filter((el) => {
     return el > 0
 })
 console.log(pairPosArr);
-
-
-
 // 7 Знайти суму парних позитивних елементів.
 let sumPairPosArr = pairPosArr.reduce((a, b) => {
     return a + b
 })
 console.log(sumPairPosArr);
-
-
 // 8 Знайти суму непарних позитивних елементів.
 let sumOddPosArr = oddPosArr.reduce((a, b) => {
     return a + b
@@ -74,11 +55,10 @@ let multiplyPosArr = positiveArr.reduce((a, b) => {
     return a * b 
 })
 console.log(multiplyPosArr);
-
 // 10 Знайти найбільший серед елементів масиву, решту обнулити.
 let resetArr = arr.map((el) => {
     if (el < arrMax) {
-        return undefined
+        return 0
     } else {
         return arrMax
     }
