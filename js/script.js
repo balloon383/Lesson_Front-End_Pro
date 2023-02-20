f6403c6d0e705d196ae06c1ff4ddbff7ff5b53cb
Anton Chekalenko <aston23112001@gmail.com>
2023-02-20 22:08:36 +0200

    lesson-8 update

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let arrNumbers
 let arrNumbersArith
 function findNum(arr, numsArith) {
 arrNumbers = arr.filter((el) => {
-    if (typeof el === `number`) {
+    if (typeof el === 'number') {
     return el;
     }
 });
@@ -20,36 +20,68 @@ function numsArith() {
 findNum(someArr, numsArith);
 
 // 2. Написати функцію doMath(x, znak, y), яка отримує 3 аргументи: числа x і y, рядок znak. У змінній znak може бути: +, -, *, /, %, ^ (ступінь ).Вивести результат математичної дії, вказаної в змінній znak.Обидва числа і знак виходять від користувача.
-/* let x = +prompt(`Write X`)
-let znak = prompt(`Write Znak`)
-let y = +prompt(`Write Y`)
+/* let x = +prompt('Write X')
+let znak = prompt('Write Znak')
+let y = +prompt('Write Y')
 doMath(x, y, znak) */
 function doMath(x, y, znak) {
-    if (typeof x !== `number`) {
-        result = `Write a NUMBER, X`;
+    if (typeof x !== 'number') {
+        result = 'Write a NUMBER, X';
     }
-    if (typeof y !== `number`) {
-        result = `Write a NUMBER, Y`;
+    if (typeof y !== 'number') {
+        result = 'Write a NUMBER, Y';
     }
-    if (znak === `+`) {
+    if (znak === '+') {
         result = x + y;
-    } else if (znak === `-`) {
+    } else if (znak === '-') {
         result = x - y;
-    } else if (znak === `*`) {
+    } else if (znak === '*') {
         result = x * y;
-    } else if (znak === `/`) {
+    } else if (znak === '/') {
         result = x / y;
-    } else if (znak === `%`) {
+    } else if (znak === '%') {
         result = x % y;
-    } else if (znak === `^`) {
+    } else if (znak === '^') {
         result = x ** y;
     } else {
-        result =  `Wrong command`;
+        result =  'Wrong command';
     }
     return result
 }
 /* alert(result) */
-// 3. Написати функцію заповнення даними користувача двомірного масиву. Довжину основного масиву і внутрішніх масивів задає користувач. Значення всіх елементів всіх масивів задає користувач.
+// 3. Написати функцію заповнення даними користувача двомірного масиву. Довжину основного масиву і внутрішніх масивів задає користувач. Значення всіх елементів всіх масивіНаписати функцію заповнення даними користувача двомірного масиву. Довжину основного масиву і внутрішніх масивів задає користувач. Значення всіх елементів всіх масивів задає користувач.
+let mainArr = []
+let mainArrLength = +prompt("enter main array length");
+let innerArrLength = +prompt("enter inner arrays length");
+validation()
+function validation() {
+    while (mainArrLength <= 0) {
+        //mainArrLength = prompt('enter valid mainArrLength')
+        console.log(typeof mainArrLength)
+        return
+    }
+    while (innerArrLength <= 0) {
+        //innerArrLength = prompt('enter valid mainArrLength')
+        console.log(typeof innerArrLength)
+        return
+    }
+    creation(mainArrLength)
+}; 
+function creation(length) {
+    for (a = 0; a < length; a++){
+        mainArr.push([])
+    }
+    addInfo()
+}
+function addInfo() {
+    for (let i = 0; i < mainArrLength; i++){
+        for (let j = 0; j < innerArrLength; j++){
+            let info = prompt(`add info to element number ${j + 1} of array number ${i + 1}`);
+            mainArr[i].push(info)
+        }
+    }
+}
+console.log(mainArr)
 
 // 4. Створити функцію, яка прибирає з рядка всі символи, які ми передали після другого аргумента. 'func("hello world", 'l', 'd')' поверне нам "heo wor". Вихідний рядок та символи для видалення задає користувач.
 // Також забезпечити логіку, при котрій після першого елементу ми можемо передати будь яким аргументом функцію, яка щось достать до нашого рядка, ГОЛОВНЕ щоб функція виконувалася після того як ми проведемо попередні маніпуляції (тобто видалимо символи).
