773d493a37085d25d3ff0af445e18d918a19f71b
Anton Chekalenko <aston23112001@gmail.com>
2023-03-02 18:54:38 +0200

    lesson-11 update

--- js/script.js
+++ js/script.js
@@ -16,3 +16,33 @@ const vegetables = [
     season: true,
   },
 ];
+
+
+class vegetable{
+  constructor(obj) {
+    this.type = `Vegetables`
+    this.seasonKoef = 1.3
+    this.name = Object.values({...obj})
+  }
+  getPrice() {
+    let total
+    if (vegetable.season == true) {
+      total = this.price * this.seasonKoef
+    }
+  }
+  getInfo() {
+    if (vegetable.season == true) {
+      document.write(`
+    Type: Vegetable. SeasonKoef: ${this.seasonKoef}. Name: ${this.name}. Icon: ${this.icon}. Price: ${this.price}. Season: true
+    `)
+    } else if (vegetable.season == false) {
+      document.write(`
+    Type: Vegetable. SeasonKoef: ${this.seasonKoef}. Name: ${this.name}. Icon: ${this.icon}. Price: ${this.price}.
+      `);
+    }
+  }
+
+}
+
+let obj = vegetables.map(el => new vegetable({ ...el }, console.log(el)))
+console.log(obj)
