2a21cb7e74d8f0be0494dc06615ec2b7fbefb967
Anton Chekalenko <aston23112001@gmail.com>
2023-03-15 14:12:45 +0200

    lesson-13 update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-const SINGL_LIST_HEAD = {
-  value: 0,
+// Singly linked list
+const SINGLE_LIST_HEAD = {
+  value: 5,
   next: null,
-  prev: null,
 };
+let singleList = [SINGLE_LIST_HEAD,];
+
+
+function funSingleList(values, link = null) {
+  let SINGLE_LIST_OBJECT = {
+    value: values,
+    next: link,
+  };
+  singleList.forEach((el) => {
+    if (el.next == null) {
+      el.next = SINGLE_LIST_OBJECT;
+      return el.next
+    }
+  })
+  singleList.push(SINGLE_LIST_OBJECT)
+}
+funSingleList(1)
+funSingleList(true)
+funSingleList(() => console.log('hello'))
+console.log(singleList)
+console.log(SINGLE_LIST_HEAD.next.value)
+SINGLE_LIST_HEAD.next.next.next.value()
