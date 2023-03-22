f50b7b33e81a3275990f8aec34713345a0d62fe3
Anton Chekalenko <aston23112001@gmail.com>
2023-03-22 16:11:02 +0200

    lesson-15 fix

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 let snitch = document.querySelector(".snitch");
+let text = document.querySelector(".text");
 snitch.style.top = 0;
 snitch.style.left = 0;
 snitch.style.height = 100 + `px`;
 snitch.style.width = 100 + `px`;
 let windowWidth = document.body.clientWidth;
 let windowHeight = document.body.clientHeight;
-let playFieldX = windowWidth/2 - 50;
-let playFieldY = windowHeight/2 - 50;
+let playFieldX = windowWidth / 2 - 50;
+let playFieldY = windowHeight / 2 - 50;
 const collectorY = limitsY();
 const collectorX = limitsX();
 
@@ -14,7 +15,6 @@ function movement(event) {
   if (event.charCode === 119 || event.charCode === 1094) {
     snitch.style.top = parseInt(snitch.style.top) - 10 + `px`;
     collectorY(-10);
-    console.log(`move to top`);
   } else if (
     event.charCode === 115 ||
     event.charCode === 1110 ||
@@ -22,29 +22,24 @@ function movement(event) {
   ) {
     snitch.style.top = parseInt(snitch.style.top) + 10 + `px`;
     collectorY(10);
-    console.log(`move to bottom`);
   } else if (event.charCode === 97 || event.charCode === 1092) {
     snitch.style.left = parseInt(snitch.style.left) - 10 + `px`;
     collectorX(-10);
-    console.log(`move to left`);
   } else if (event.charCode === 100 || event.charCode === 1074) {
     snitch.style.left = parseInt(snitch.style.left) + 10 + `px`;
     collectorX(10);
-    console.log(`move to right`);
   } else if (event.charCode === 32) {
     snitch.style.top = parseInt(snitch.style.top) - 10 + `px`;
     setTimeout(() => {
       snitch.style.top = parseInt(snitch.style.top) + 10 + `px`;
     }, 400);
-    console.log(`jump`);
   } else if (event.keyCode === 17) {
     snitch.style.width = 125 + `px`;
     snitch.style.height = 60 + `px`;
     document.addEventListener(`keyup`, (event) => {
-      snitch.style.width =  100 + `px`;
+      snitch.style.width = 100 + `px`;
       snitch.style.height = 100 + `px`;
     });
-    console.log(`crouch`);
   }
 }
 function limitsX() {
@@ -52,23 +47,26 @@ function limitsX() {
 
   function moveX(number) {
     limsX += number;
-    console.log(`current posX: ${limsX}`);
     if (limsX >= playFieldX) {
       limsX -= number;
+      text.style.color = `red`;
       setTimeout(() => {
-          snitch.style.left = parseInt(snitch.style.left) - 20 + `px`;
-      limsX -= number;
-          
+        snitch.style.left = parseInt(snitch.style.left) - 20 + `px`;
+        limsX -= number;
       }, 200);
-      console.log(`current posX: ${limsX}`);
+      setTimeout(() => {
+        text.style.color = `black`;
+      }, 2000);
     } else if (limsX <= -playFieldX) {
       limsX -= number;
+      text.style.color = `red`;
       setTimeout(() => {
-          snitch.style.left = parseInt(snitch.style.left) + 20 + `px`;
-      limsX -= number;
-          
+        snitch.style.left = parseInt(snitch.style.left) + 20 + `px`;
+        limsX -= number;
       }, 200);
-      console.log(`current posX: ${limsX}`);
+      setTimeout(() => {
+        text.style.color = `black`;
+      }, 2000);
     }
   }
   return moveX;
@@ -78,21 +76,26 @@ function limitsY() {
 
   function moveX(number) {
     limsY += number;
-    console.log(`current pos: ${limsY}`);
     if (limsY >= playFieldY) {
       limsY -= number;
+      text.style.color = `red`;
       setTimeout(() => {
-          snitch.style.top = parseInt(snitch.style.top) - 20 + `px`;
-          limsY -= number;
+        snitch.style.top = parseInt(snitch.style.top) - 20 + `px`;
+        limsY -= number;
       }, 200);
-      console.log(`current posX: ${limsY}`);
+      setTimeout(() => {
+        text.style.color = `black`;
+      }, 2000);
     } else if (limsY <= -playFieldY) {
       limsY -= number;
+      text.style.color = `red`;
       setTimeout(() => {
-          snitch.style.top = parseInt(snitch.style.top) + 20 + `px`;
-          limsY -= number;
+        snitch.style.top = parseInt(snitch.style.top) + 20 + `px`;
+        limsY -= number;
       }, 200);
-      console.log(`current posX: ${limsY}`);
+      setTimeout(() => {
+        text.style.color = `black`;
+      }, 2000);
     }
   }
   return moveX;
@@ -100,4 +103,3 @@ function limitsY() {
 
 document.addEventListener(`keypress`, (event) => movement(event));
 document.addEventListener(`keydown`, (event) => movement(event));
-
