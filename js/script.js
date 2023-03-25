ae18afaa4b9aedccf5fa4afa7c27fc82337d757e
Anton Chekalenko <aston23112001@gmail.com>
2023-03-25 14:50:46 +0200

    lesson-16 update

--- js/script.js
+++ js/script.js
@@ -1,23 +1,27 @@
-const BURGER_SMALL = `small`
-const BURGER_LARGE = `large`
-const STUFFING_CHEESE = `cheese`
-const STUFFING_SALAD = `salad`
-const STUFFING_POTATO = `potato`
-const TOPPING_MAYO = `mayo`
-const TOPPING_SPICES = `spices`
+const BURGER_SMALL = `small`;
+const BURGER_LARGE = `large`;
+const STUFFING_CHEESE = `cheese`;
+const STUFFING_SALAD = `salad`;
+const STUFFING_POTATO = `potato`;
+const TOPPING_MAYO = `mayo`;
+const TOPPING_SPICES = `spices`;
 let userName = document.querySelector(`.input1`);
 let burgerSize = document.querySelector(`.input2`);
 let burgerStuffing = document.querySelectorAll(`.input3`);
 let burgerTopping = document.querySelectorAll(`.input4`);
 let burgerInfo = document.querySelector(`.input5`);
 let orderButton = document.querySelector(`.mac__button`);
-let orderField = document.querySelector(`.mac__list`)
+let orderField = document.querySelector(`.mac__list`);
+let macForm = document.querySelector(`.mac__form`)
 orderButton.addEventListener(`click`, (event) => {
-  event.preventDefault()
-  defineProperties(burgerStuffing, burgerTopping);
+  event.preventDefault();
+  defineProperties(burgerSize, burgerStuffing, burgerTopping);
+  macForm.reset()
 });
-
-function defineProperties(stuffing, topping){
+function defineProperties(size, stuffing, topping) {
+  let defineSize;
+  let defineStuffing;
+  let defineTopping;
   const addBurgerStuffing = [...stuffing].filter((el) => {
     if (el.checked) {
       return el.value;
@@ -28,9 +32,26 @@ function defineProperties(stuffing, topping){
       return el.value;
     }
   });
-  makeOrder(burgerSize.value, addBurgerStuffing[0].value, addBurgerTopping);
-}
 
+  if (size.value === `large`) {
+    defineSize = BURGER_LARGE;
+  } else {
+    defineSize = BURGER_SMALL;
+  }
+  if (addBurgerStuffing[0].value === `cheese`) {
+    defineStuffing = STUFFING_CHEESE;
+  } else if (addBurgerStuffing[0].value === `potato`) {
+    defineStuffing = STUFFING_POTATO;
+  } else {
+    defineStuffing = STUFFING_SALAD;
+  }
+
+  defineTopping = addBurgerTopping.map((el) => {
+    return el.value;
+  });
+
+  makeOrder(defineSize, defineStuffing, defineTopping);
+}
 
 class Hamburger {
   constructor(Hamburger) {
@@ -39,16 +60,16 @@ class Hamburger {
     this.topping = Hamburger.topping;
   }
   set addTopping(topping) {
-    this.topping = topping
+    this.topping = topping;
   }
 
   getPrice() {
-    let price = this.#burgerPrice
-    return price
+    let price = this.#burgerPrice;
+    return price;
   }
   getCalories() {
-    let calories = this.#burgerCalories
-    return calories
+    let calories = this.#burgerCalories;
+    return calories;
   }
   get #burgerPrice() {
     let totalPrice;
@@ -73,7 +94,7 @@ class Hamburger {
         totalPrice = totalPrice + 15;
       }
     } else if (this.topping.length >= 2) {
-      for (let i = 0; i < this.topping.length; i++){
+      for (let i = 0; i < this.topping.length; i++) {
         if (this.topping[i] == `mayo`) {
           totalPrice = totalPrice + 20;
         } else if (this.topping[i] == `spices`) {
@@ -81,8 +102,8 @@ class Hamburger {
         }
       }
     }
-    
-    return totalPrice
+
+    return totalPrice;
   }
 
   get #burgerCalories() {
@@ -116,53 +137,47 @@ class Hamburger {
         }
       }
     }
-    return totalCalories
+    return totalCalories;
   }
 }
 
-
-function makeOrder(size, stuffing, topping,) {
-  let burgerSize
-  let burgerStuffing
-  let burgerTopping
-  if (size === `large`) {
-    burgerSize = BURGER_LARGE;
-  } else {
-    burgerSize = BURGER_SMALL;
-  }
-
-  if (stuffing === `cheese`) {
-    burgerStuffing = STUFFING_CHEESE
-  } else if (stuffing === `potato`) {
-    burgerStuffing = STUFFING_POTATO;
-  } else {
-    burgerStuffing = STUFFING_SALAD;
-  }
-
-  burgerTopping = topping.map((el) => {
-    return el.value
-  })
+function makeOrder(size, stuffing, topping) {
   let burger = new Hamburger({
-    size: burgerSize,
-    type: burgerStuffing,
-    topping: burgerTopping
+    size: size,
+    type: stuffing,
+    topping: topping,
   });
-  orderLog(userName.value, burger.size, burger.type, burger.topping, burger.getPrice(), burger.getCalories());
+  orderLog(
+    userName.value,
+    burger.size,
+    burger.type,
+    burger.topping,
+    burger.getPrice(),
+    burger.getCalories()
+  );
 }
-
 
 function orderLog(name, size, stuffing, topping, price, calories) {
-  let randomValue = (Math.random() * 10).toFixed(1)
-  let message = document.createElement(`div`)
-  message.innerHTML = 
-      `<div class = 'mac__order'>
+  let randomValue = (Math.random() * 10).toFixed(1);
+  let message = document.createElement(`div`);
+  if (burgerInfo.value.length === 0) {
+    message.innerHTML = `<div class = 'mac__order'>
         Привет ${name}
         <br>
         Ваш заказ ${size} бургер с ${stuffing} и ${topping} будет готов в течении ${randomValue} минут.
         <br>
         Стоимость заказа: ${price} ( ${calories} calories)
       </div>`;
-  orderField.appendChild(message)    
+  } else if (burgerInfo.value.length >= 1) {
+    message.innerHTML = `<div class = 'mac__order'>
+        Привет ${name}
+        <br>
+        Ваш заказ ${size} бургер с ${stuffing} и ${topping} будет готов в течении ${randomValue} минут.
+        <br>
+        Стоимость заказа: ${price} ( ${calories} calories)
+        Дополнительная информация к заказу: ${burgerInfo.value}
+      </div>`;
+  }
+    
+  orderField.appendChild(message);
 }
-
-
