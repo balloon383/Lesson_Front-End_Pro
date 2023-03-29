e3529bf02e0a1b8029efb45768b65a4182b69042
Anton Chekalenko <aston23112001@gmail.com>
2023-03-29 12:01:08 +0300

    lesson-17 update

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const userData = {
+  USD: 1000,
+  EUR: 900,
+  UAH: 15000,
+  BIF: 20000,
+  AOA: 100,
+};
+
+const bankData = {
+  USD: {
+    max: 3000,
+    min: 100,
+    img: "💵",
+  },
+  EUR: {
+    max: 1000,
+    min: 50,
+    img: "💶",
+  },
+  UAH: {
+    max: 0,
+    min: 0,
+    img: "💴",
+  },
+  GBP: {
+    max: 10000,
+    min: 100,
+    img: "💷",
+  },
+};
+
+let checker = confirm(`Want to check your balance?`)
+function checkRequest(){
+
+}
