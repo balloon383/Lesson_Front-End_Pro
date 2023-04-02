63bb3d74090b6b9dc73d645496faa6934a16333e
Anton Chekalenko <aston23112001@gmail.com>
2023-04-02 14:27:47 +0300

    lesson-18 update

--- js/script.js
+++ js/script.js
@@ -1,17 +1,59 @@
-let randomJokeSelector = document.querySelector(".jokes__radio");
-let categoriesJokeSelector = document.querySelectorAll(".jokes__categories--category");
+let jokesRadio = document.querySelectorAll(".jokes__radio");
+let randomJokeSelector = document.querySelector(".jokes__radio--random");
+let categoriesJokeSelector = document.querySelectorAll(
+  ".jokes__categories--category"
+);
 let searchJokeSelector = document.querySelector(".jokes__search--input");
 let sendButtonSelector = document.querySelector(".jokes__get--button");
 let jokesForm = document.querySelector(".jokes__form");
 sendButtonSelector.addEventListener("click", (event) => {
-  console.log(randomJokeSelector.checked);
-  let categoriesJoke = [...categoriesJokeSelector].filter((el) => {
+  let chosenValue = [...jokesRadio].filter((el) => {
     if (el.checked) {
       return el;
     }
   });
-  console.log(categoriesJokeSelector[0].value);
-  console.log(searchJokeSelector.value);
+  valueValidation(chosenValue);
   event.preventDefault();
-  jokesForm.reset();
 });
+
+function valueValidation(value) {
+  let getterValue;
+  if (value[0].value === "random") {
+    getterValue = "random";
+  } else if (value[0].value === "categories") {
+    getterValue = [...categoriesJokeSelector].filter((el) => {
+      if (el.checked) {
+        return el;
+      }
+    });
+    getterValue = getterValue[0].value;
+  } else if (value[0].value === "search") {
+    getterValue = searchJokeSelector.value;
+  }
+  getJokes(value, getterValue);
+  jokesForm.reset();
+}
+let getJokes = (value, keyWord) => {
+  let newJoke;
+  if (value[0].value === "random") {
+    fetch(`https://api.chucknorris.io/jokes/random`).then((result) => {
+      console.log(result);
+    });
+  } else if (value[0].value === "categories") {
+    fetch(`https://api.chucknorris.io/jokes/random?category=${keyWord}`).then(
+      (result) => {
+        console.log(result);
+      }
+    );
+  } else if (value[0].value === "search") {
+    fetch(`https://api.chucknorris.io/jokes/search?query=${keyWord}`).then(
+      (result) => {
+        console.log(result);
+      }
+    );
+  }
+};
+function createJoke(joke) {
+  let jokeBlock = document.createElement("div");
+  jokeBlock.innerText;
+}
