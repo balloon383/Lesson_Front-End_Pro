03982f29caf1a3f3a855c6fd2da7856389083d92
balloon383 <aston23112001@gmail.com>
2023-04-27 23:27:44 +0300

    lesson-26 update

---
+++ app/js/script.js
@@ -0,0 +1,8 @@
+let categoryField = document.querySelector('.main__container')
+createElements()
+async function createElements(){
+    let getElements = await (() =>{
+        fetch('https://634e9f834af5fdff3a625f84.mockapi.io/products')
+        
+    })
+}
