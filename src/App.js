c93bde5ef1c24cb45cbc93116f0b8e1520270196
Anton Chekalenko <aston23112001@gmail.com>
2023-05-19 13:04:42 +0300

    lesson-32 changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import './App.css';
 import Login from './pages/login/Login';
+import Main from './pages/mainPage/MainPage'
+import User from './pages/user/User'
+import ShoppingCart from './pages/shoppingCart/ShoppingCart'
 export default function App() {
 
   const [state, setState] = useState('login')
 
+  function changePage(pageName = 'login') {
+
+    if (pageName === 'login') {
+      setState('login')
+    } else if (pageName === 'main') {
+      setState("main");
+    } else if (pageName === 'user') {
+      setState('user')
+    } else if (pageName === 'shoppingCart') {
+      setState("shoppingCart");
+    }
+
+    console.log(state)
+
+  }
+
   return (
     <div className="App">
-      {state === 'login' ? <Login/> : <></>}
+      {state === "login" ? <Login /> : state === "main" ? <Main/> : state === 'user' ? <User/> : <ShoppingCart/>}
     </div>
   );
   
---
+++ src/pages/mainPage/MainPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../../components/header/header'
+
+export default function MainPage() {
+  return (
+      <section>
+          <Header/>
+          MainPage
+      </section>
+  )
+}
