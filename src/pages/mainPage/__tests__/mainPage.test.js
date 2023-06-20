b79ada62b586fe4f0b9f158a79f407b8ffc85208
Anton Chekalenko <aston23112001@gmail.com>
2023-06-20 23:48:04 +0300

    lesson-38 update

--- src/pages/mainPage/__tests__/mainPage.test.js
+++ src/pages/mainPage/__tests__/mainPage.test.js
@@ -1,9 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
-import configureStore from "redux-mock-store";
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+
 import { MainPage } from "../MainPage";
 
-const mockStore = configureStore([]);
+const mockStore = configureMockStore([thunk]);
 
 describe("MainPage", () => {
   let store;
@@ -12,7 +14,6 @@ describe("MainPage", () => {
     store = mockStore({
       products: [
         { id: 1, category: "Category 1" },
-        { id: 2, category: "Category 1" },
         { id: 3, category: "Category 2" },
       ],
     });
@@ -32,21 +33,5 @@ describe("MainPage", () => {
     expect(category2Title).toBeInTheDocument();
   });
 
-  test("renders products for each category", () => {
-    render(
-      <Provider store={store}>
-        <MainPage />
-      </Provider>
-    );
-
-    const category1Products = screen.getAllByTestId(
-      "category-products-Category 1"
-    );
-    const category2Products = screen.getAllByTestId(
-      "category-products-Category 2"
-    );
-
-    expect(category1Products).toHaveLength(2);
-    expect(category2Products).toHaveLength(1);
-  });
+  
 });
